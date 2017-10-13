c6bde9c456db7f44bddd037c92291b1973efdf64
Justin van Gemen <20277@ma-web.nl>
2017-10-13 14:12:03 +0200

    Making the map a bit more interesting

--- Assets/_Scripts/PlayerEat.cs
+++ Assets/_Scripts/PlayerEat.cs
@@ -5,24 +5,21 @@ public class PlayerEat : MonoBehaviour {
 
 	private GameObject _tempFood;
 	private string _food;
-	private Shader _shader;
-	private float _fatness;
+	private Renderer _renderer;
+	private float _ext;
 
 	private void Start()
 	{
-		_shader = GetComponent<Shader>();
+		_renderer = GetComponent<Renderer>();
 		StartCoroutine("Weightloss");
 	}
 
 	private void OnTriggerStay (Collider other)
 	{
-		if (!other.CompareTag("Edible")) return;
-		if (other.name == "Food")
-		{		
-			_fatness = _fatness + 0.01f;
-			//_shader.material.SetFloat("Extrusion", _fatness);
-		}
-		other.name = "Eaten";
+		if(!other.CompareTag("Edible")) return;
+		Debug.Log("Eating" + _ext);
+		_ext = _ext +0.05f;
+		_renderer.material.SetFloat("_Amount", _ext);
 		_tempFood = other.gameObject;
 		StartCoroutine ("Eat");
 	}
@@ -34,9 +31,10 @@ public class PlayerEat : MonoBehaviour {
 
 	private IEnumerator Weightloss()
 	{
-		_fatness = _fatness - 0.001f;
-		//_shader.material.SetFloat("Extrustion", _fatness);
-		yield return new WaitForSeconds(0.5f);
+		Debug.Log("Weightloss" + _ext);
+		_ext = _ext - 0.002f;
+		_renderer.material.SetFloat("_Amount", _ext);
+		yield return new WaitForSeconds(2f);
 		StartCoroutine("Weightloss");
 	}
 }
